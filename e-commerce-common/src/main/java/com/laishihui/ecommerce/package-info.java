7dcbfc1a3c27846b05cee533d33463c11c9affe2
tachai <1206966083@qq.com>
2021-12-25 21:22:21 +0800

    电商微服务学习

---
+++ e-commerce-common/src/main/java/com/laishihui/ecommerce/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Create by tachai on 2021/12/25 9:21 下午
+ * gitHub https://github.com/TACHAI
+ * Email tc1206966083@gmail.com
+ */
+package com.laishihui.ecommerce;
---
+++ e-commerce-common/src/main/java/com/laishihui/ecommerce/vo/CommonResponse.java
@@ -0,0 +1,9 @@
+package com.laishihui.ecommerce.vo;
+
+/**
+ * Create by tachai on 2021/12/25 9:21 下午
+ * gitHub https://github.com/TACHAI
+ * Email tc1206966083@gmail.com
+ */
+public class CommonResponse {
+}
