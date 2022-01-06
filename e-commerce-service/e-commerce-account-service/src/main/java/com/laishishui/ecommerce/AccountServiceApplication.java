278f50739249b318553713fe520ff25372ad53a4
tachai <1206966083@qq.com>
2022-01-06 17:02:01 +0800

    用户账户微服务模块初始化

---
+++ e-commerce-service/e-commerce-account-service/src/main/java/com/laishishui/ecommerce/AccountServiceApplication.java
@@ -0,0 +1,23 @@
+package com.laishishui.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+/**
+ * <h1>用户账户微服务启动入口</h1>
+ * 127.0.0.1:8003/ecommerce-account-service/doc.html
+ * @author tachai on 2022/1/6 4:56 下午
+ * gitHub https://github.com/TACHAI
+ * Email tc1206966083@gmail.com
+ */
+@EnableJpaAuditing
+@EnableDiscoveryClient
+@SpringBootApplication
+public class AccountServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountServiceApplication.class,args);
+    }
+}
